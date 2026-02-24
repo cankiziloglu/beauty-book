22640c6df461d3ff5229bf9883e3b7f30dff8f70
Can Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2026-02-24 13:50:17 +0300

    fix folder path ref

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
-import { auth } from "./lib/auth";
+import { auth } from "@/lib/auth";
+import { logger } from "hono/logger";
 
 const app = new Hono().basePath("/api");
 
+app.use(logger());
 app.on(["POST", "GET"], "/auth/*", (c) => auth.handler(c.req.raw));
 
 app.get("/", (c) => {
