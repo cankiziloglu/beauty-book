42f9f348023219c0cee64035015832825ff4e3aa
Can Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2026-02-27 09:18:02 +0300

    create db schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,5 +1,16 @@
 import { relations } from "drizzle-orm";
-import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  timestamp,
+  boolean,
+  index,
+  integer,
+  primaryKey,
+  pgEnum,
+  time,
+  date,
+} from "drizzle-orm/pg-core";
 
 // AUTH SCHEMA
 
@@ -97,9 +108,171 @@ export const accountRelations = relations(account, ({ one }) => ({
 
 // APP SCHEMA
 
-// export const clinic = pgTable(
-//   "clinic",
-//   {
-//     id:
-//   }
-// )
+export const clinic = pgTable("clinic", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  email: text().notNull().unique(),
+  phoneNumber: text().notNull(),
+  address: text().notNull(),
+  timezone: text().notNull(),
+  minAdvanceHours: integer().notNull(),
+  maxAdvanceDays: integer().notNull(),
+  cancellationHours: integer().notNull(),
+  isActive: boolean().notNull().default(true),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  adminId: text()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+});
+
+export const practitioner = pgTable("practitioner", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  bio: text().notNull(),
+  isActive: boolean().notNull().default(true),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  clinicId: integer()
+    .notNull()
+    .references(() => clinic.id, { onDelete: "cascade" }),
+});
+
+export const treatment = pgTable("treatment", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  description: text(),
+  duration: integer().notNull(),
+  buffer: integer().notNull(),
+  priceCents: integer().notNull(),
+  isActive: boolean().notNull().default(true),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  clinicId: integer()
+    .notNull()
+    .references(() => clinic.id, { onDelete: "cascade" }),
+});
+
+export const practitionerTreatment = pgTable(
+  "practitionerTreatment",
+  {
+    createdAt: timestamp().notNull(),
+    updatedAt: timestamp()
+      .$onUpdate(() => new Date())
+      .notNull(),
+    practitionerId: integer()
+      .notNull()
+      .references(() => practitioner.id, { onDelete: "cascade" }),
+    treatmentId: integer()
+      .notNull()
+      .references(() => treatment.id, { onDelete: "cascade" }),
+  },
+  (table) => [
+    primaryKey({ columns: [table.practitionerId, table.treatmentId] }),
+  ],
+);
+
+export const room = pgTable("room", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  isActive: boolean().notNull().default(true),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  clinicId: integer()
+    .notNull()
+    .references(() => clinic.id, { onDelete: "cascade" }),
+});
+
+export const roomTreatment = pgTable(
+  "roomTreatment",
+  {
+    createdAt: timestamp().notNull(),
+    updatedAt: timestamp()
+      .$onUpdate(() => new Date())
+      .notNull(),
+    roomId: integer()
+      .notNull()
+      .references(() => room.id, { onDelete: "cascade" }),
+    treatmentId: integer()
+      .notNull()
+      .references(() => treatment.id, { onDelete: "cascade" }),
+  },
+  (table) => [primaryKey({ columns: [table.roomId, table.treatmentId] })],
+);
+
+export const dowEnum = pgEnum("dayOfWeek", ["1", "2", "3", "4", "5", "6", "7"]);
+
+export const availability = pgTable("availability", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  dayOfWeek: dowEnum(),
+  startTime: time({ withTimezone: true }).notNull(),
+  endTime: time({ withTimezone: true }).notNull(),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  practitionerId: integer()
+    .notNull()
+    .references(() => practitioner.id, { onDelete: "cascade" }),
+});
+
+export const unavailability = pgTable("unavailability", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  dayOfWeek: dowEnum(),
+  startTime: time({ withTimezone: true }).notNull(),
+  endTime: time({ withTimezone: true }).notNull(),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  practitionerId: integer()
+    .notNull()
+    .references(() => practitioner.id, { onDelete: "cascade" }),
+});
+
+export const statusEnum = pgEnum("status", [
+  "confirmed",
+  "cancelled",
+  "no-show",
+  "completed",
+  "late-cancel",
+]);
+
+export const appointment = pgTable("appointment", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  date: date().notNull(),
+  startTime: time({ withTimezone: true }).notNull(),
+  endTime: time({ withTimezone: true }).notNull(),
+  bufferEndTime: time({ withTimezone: true }).notNull(),
+  status: statusEnum(),
+  notes: text(),
+  priceCents: integer().notNull(),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  clinicId: integer()
+    .notNull()
+    .references(() => clinic.id, { onDelete: "no action" }),
+  clientId: text()
+    .notNull()
+    .references(() => user.id, { onDelete: "no action" }),
+  practitionerId: integer()
+    .notNull()
+    .references(() => practitioner.id, { onDelete: "no action" }),
+  treatmentId: integer()
+    .notNull()
+    .references(() => treatment.id, { onDelete: "no action" }),
+  roomId: integer()
+    .notNull()
+    .references(() => room.id, { onDelete: "no action" }),
+});
