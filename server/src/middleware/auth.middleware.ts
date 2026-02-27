b94877b181f4b8a4ff26e070bc74f259d856b51a
Can Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2026-02-27 13:10:37 +0300

    add auth middleware

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,15 @@
+import { auth } from "@/lib/auth";
+import { createMiddleware } from "hono/factory";
+
+export const authMiddleware = createMiddleware(async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    await next();
+    return;
+  }
+  c.set("user", session.user);
+  c.set("session", session.session);
+  await next();
+});
