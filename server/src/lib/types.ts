4c2f7a4f2ae362d8c18308de3f8c3b8dacb9ff6a
Can Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2026-02-27 09:18:42 +0300

    add type for auth

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,7 +1,16 @@
+import { auth } from "./auth";
+
 export type SendEmail = {
   to: string;
   subject: string;
   url: string;
   type: "verifying" | "restoring";
   user: string;
+};
+
+export type AuthType = {
+  Variables: {
+    session: typeof auth.$Infer.Session.session | null;
+    user: typeof auth.$Infer.Session.user | null;
+  };
 };
