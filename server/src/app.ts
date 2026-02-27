a24a408881bee485b3fac77ae2046f550c1f2078
Can Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2026-02-27 09:20:19 +0300

    add routes to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,29 @@
 import { Hono } from "hono";
-import { auth } from "./lib/auth";
 import { logger } from "hono/logger";
+import onError from "./middleware/error";
+import clinic from "./routes/clinic.route";
+import appointment from "./routes/appointment.route";
+import practitioner from "./routes/practitioner.route";
+import treatment from "./routes/treatment.route";
+import room from "./routes/room.route";
 
-const app = new Hono().basePath("/api");
+const app = new Hono({ strict: false }).basePath("/api");
 
 app.use(logger());
-app
-  .on(["POST", "GET"], "/auth/*", (c) => auth.handler(c.req.raw))
-  .get("/", (c) => {
-    return c.text("Hello Hono!");
-  });
 
+app.notFound((c) => {
+  return c.json({
+    message: "404 Not Found",
+    path: `${c.req.path}`,
+  });
+});
+app.onError(onError);
+
+const routes = [clinic, appointment, practitioner, treatment, room] as const;
+
+routes.forEach((route) => {
+  app.route("/", route);
+});
+
+export type AppType = (typeof routes)[number];
 export default app;
