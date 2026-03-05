f5c751f1e683e6d5cd4fd804d5a67e6dde2a4b45
Can Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2026-03-05 13:52:06 +0300

    improve auth middleware

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,15 +1,15 @@
 import { auth } from "@/lib/auth";
+import { AuthType } from "@/lib/types";
 import { createMiddleware } from "hono/factory";
+import { HTTPException } from "hono/http-exception";
 
-export const authMiddleware = createMiddleware(async (c, next) => {
+export const authMiddleware = createMiddleware<AuthType>(async (c, next) => {
   const session = await auth.api.getSession({ headers: c.req.raw.headers });
   if (!session) {
-    c.set("user", null);
-    c.set("session", null);
-    await next();
-    return;
+    throw new HTTPException(401, { message: "Unauthorized" });
+  } else {
+    c.set("user", session.user);
+    c.set("session", session.session);
   }
-  c.set("user", session.user);
-  c.set("session", session.session);
   await next();
 });
